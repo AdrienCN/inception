8ebc880e5102323a7cb65e65faf46ebf9428b8bd
Adrien <adrien.consta@gmail.com>
2021-06-14 18:32:53 +0200

    auto-commit

--- srcs/mariadb/script/init_mariadb.sh
+++
@@ -1,8 +0,0 @@
-echo "CREATE DATABASE $WP_DB_NAME;" | mysql -u root                     
-echo "GRANT ALL ON $WP_DB_NAME.* TO '$WP_DB_USER'@'$WP_DB_HOST' IDENTIFIED BY '$WP_DB_PASSWORD' WITH GRANT OPTION;" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-echo "$WP_DB_NAME" 
-echo "$WP_DB_PASSWORD"
-
-bash
